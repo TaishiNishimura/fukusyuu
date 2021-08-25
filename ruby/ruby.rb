0885c94b246fca05bc075b9933b90cd4dd078609
TaishiNishimura <taishi19880324@gmail.com>
2021-08-25 10:23:55 +0000

    [8/25]

---
+++ ruby/ruby.rb
@@ -0,0 +1,24 @@
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum += n
+end
+puts sum 
+
+a = [1, 2, 3, 1, 2, 3]
+a.delete(2)
+puts a
+
+a = [1, 2, 3, 1, 2, 3]
+a.delete_if do |n|
+ n.odd?
+end
+puts a
+
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n * 10 : n
+  sum += sum_value
+end 
+puts sum 
