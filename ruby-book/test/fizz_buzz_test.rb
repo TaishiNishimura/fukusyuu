f268f567f375f6aa2e69879d539352a905a6a412
TaishiNishimura <taishi19880324@gmail.com>
2021-08-26 06:56:30 +0000

    [8/26.3]

--- ruby-book/test/fizz_buzz_test.rb
+++ ruby-book/test/fizz_buzz_test.rb
@@ -1,16 +1,5 @@
-def fizz_buzz(number)
-  if number % 15 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else
-    number.to_s
-  end
-end
-
 require 'minitest/autorun'
+require './lib/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
@@ -23,10 +12,3 @@ class FizzBuzzTest < Minitest::Test
     assert_equal 'FizzBuzz', fizz_buzz(15)
   end
 end
-
-#puts "数字を入力してください。"
-
-#input = gets.to_i
-
-#puts "結果は..."
-#puts fizz_buzz(input)
